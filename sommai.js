7251d2c7fb2a22e40e4771156f6308e2bf3a59a1
narutoskp <dragonskp@gmail.com>
2016-12-20 15:41:24 +0700

    frist commit

---
+++ sommai.js
@@ -0,0 +1,43 @@
+$(window).scroll(function () {
+    var navber = $('.sticky-header'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= 200) navber.addClass('fixed');
+    else navber.removeClass('fixed');
+});
+
+// $('#contact-form').bootstrapValidator({
+// //        live: 'disabled',
+//     message: 'This value is not valid',
+//     feedbackIcons: {
+//         valid: 'glyphicon glyphicon-ok',
+//         invalid: 'glyphicon glyphicon-remove',
+//         validating: 'glyphicon glyphicon-refresh'
+//     },
+//     fields: {
+//         Name: {
+//             validators: {
+//                 notEmpty: {
+//                     message: 'The Name is required and cannot be empty'
+//                 }
+//             }
+//         },
+//         email: {
+//             validators: {
+//                 notEmpty: {
+//                     message: 'The email address is required'
+//                 },
+//                 emailAddress: {
+//                     message: 'The email address is not valid'
+//                 }
+//             }
+//         },
+//         Message: {
+//             validators: {
+//                 notEmpty: {
+//                     message: 'The Message is required and cannot be empty'
+//                 }
+//             }
+//         }
+//     }
+// });
